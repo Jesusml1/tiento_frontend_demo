032936b94f9911ce28d3647d2df1470431685f0b
jesus <jesus_lopez1997@hotmail.com>
2023-04-24 10:23:22 -0400

    chore: remove commented code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 import VerifyEmail from "./pages/Verify";
 import Dashboard from "./pages/Dashboard";
-// import { useUserAuth } from "./hooks/useUserAuth";
 
 function App() {
   return (
@@ -18,16 +17,5 @@ function App() {
     </Routes>
   );
 }
-
-// function RequireAuth({ children }: { children: JSX.Element }) {
-//   const { user } = useUserAuth();
-//   let location = useLocation();
-
-//   if (!user) {
-//     return <Navigate to="/" state={{ from: location }} replace />;
-//   }
-
-//   return children;
-// }
 
 export default App;
